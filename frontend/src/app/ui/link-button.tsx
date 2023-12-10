37d5c555c7568cddfe46a6e08d93fc806ca45db1
Ash <57868096+NotVeryAsh@users.noreply.github.com>
2023-12-10 14:37:55 +0000

    Add dropshadows and hover transitions on buttons and add text to import google calendar button

--- frontend/src/app/ui/link-button.tsx
+++ frontend/src/app/ui/link-button.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function LinkButton({ href, text, color }: { href: string, text: string, color: string }) {
 
-    const style = color + " text-white text-center px-4 py-2 rounded-md";
+    const style = color + " text-white text-center px-4 py-2 rounded-md drop-shadow-sm transition ease-in-out hover:-translate-y-1";
 
     return (
         <Link href={href} className={style}>
