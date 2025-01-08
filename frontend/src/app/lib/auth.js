a95115d4dfeea89755892eefd2592db00ff82751
Ash <ash@getuplist.com>
2025-01-07 17:25:43 -0800

    Fix token not getting stored in cookies

--- frontend/src/app/lib/auth.js
+++ frontend/src/app/lib/auth.js
@@ -6,14 +6,17 @@ import { cookies } from 'next/headers'
 export async function setToken(token) {
 
     // The expiration date is 5 minutes before google expires the token
-    const expiresAt = new Date(Date.now() + (token.expires_in - (60 * 5)))
+    const expiresAt = new Date();
+    expiresAt.setTime(expiresAt.getTime() + ((token.expires_in * 1000) - (1000 * 60 * 5)))
+    expiresAt.toUTCString()
+    
     const cookieStore = await cookies()
 
-    cookieStore.set('token', token.access_token, {
-        httpOnly: true,
-        secure: true,
+    cookieStore.set('token', token.access_token,{
+        name: 'token',
+        value: token.access_token,
         expires: expiresAt,
-        sameSite: 'lax',
+        sameSite: 'strict',
         path: '/',
     })
 }
