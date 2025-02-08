216559439aee652abcbb870f0d0297e3920844fd
ashp8 <a.phillips7@outlook.com>
2025-02-08 13:47:17 -0800

    Adjust attributes for copy text component so it's more reactive when the user hovers / clicks  interacts with the input

--- frontend/src/app/components/CopyText.jsx
+++ frontend/src/app/components/CopyText.jsx
@@ -4,14 +4,14 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 export default function CopyText({children})
 {
     return (
-        <div className={"flex p-2 bg-gray-100 rounded"} onClick={handleClickComponent}>
+        <div className={"flex p-2 bg-gray-100 rounded hover:cursor-pointer hover:bg-gray-200"} onClick={handleClickComponent}>
             <div
                 className={"flex text-center truncate m-auto mr-2"}>
                 {children}
             </div>
-            <div className={"flex flex-shrink-0 w-10 h-10 rounded bg-gray-200"} onClick={(e) => {handleClickCopy(e, children)}}>
+            <button className={"flex flex-shrink-0 w-10 h-10 rounded bg-gray-200 hover:bg-gray-300 active:bg-gray-400"} onClick={(e) => {handleClickCopy(e, children)}}>
                 <FontAwesomeIcon icon={faCopy} className={"m-auto text-xl"}/>
-            </div>
+            </button>
         </div>
     )
 }
@@ -25,6 +25,9 @@ const handleClickComponent = (e) => {
 }
 
 const handleClickCopy = (e, text) => {
+    if(text === null) {
+        return;
+    }
     navigator.clipboard.writeText(text).then(() => {
         // TODO Show popup 'Link copied!'
     })
